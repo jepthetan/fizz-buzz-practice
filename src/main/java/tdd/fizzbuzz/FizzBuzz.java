d3d651a4c8695b050a5c0fc9e43a1a208188515d
JEPTHE TAN (DOCU-IRIS-ISD-OOCLL/MNL) <TANJE5@OOCL.COM>
2022-09-01 14:46:28 +0800

    feat: add multiple of five seven order return

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,7 +4,8 @@ public class FizzBuzz {
     public String countOff (int order) {
         if (order % 3 == 0 && order % 5 == 0 && order % 7 == 0) {
             return "FizzBuzzWhizz";
-        } else if (order % 3 == 0 && order % 5 == 0) {
+        } else if (order % 3 == 0 && order % 5 == 0
+                || order % 5 == 0 && order % 7 == 0) {
             return "FizzBuzz";
         } else if (order % 3 == 0) {
             return "Fizz";
