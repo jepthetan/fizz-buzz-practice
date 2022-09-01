cb291e6c50a37411cffaad979197f9313d1cd727
JEPTHE TAN (DOCU-IRIS-ISD-OOCLL/MNL) <TANJE5@OOCL.COM>
2022-09-01 14:30:59 +0800

    feat: add multiple of three five seven  order return

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,7 +2,9 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff (int order) {
-        if (order % 3 == 0) {
+        if (order % 3 == 0 && order % 5 == 0 && order % 7 == 0) {
+            return "FizzBuzzWhizz";
+        } else if (order % 3 == 0) {
             return "Fizz";
         } else if (order % 5 == 0) {
             return "Buzz";
