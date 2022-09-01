cb52aba199478685c9d6b3049cd0dbae5f1fdb33
JEPTHE TAN (DOCU-IRIS-ISD-OOCLL/MNL) <TANJE5@OOCL.COM>
2022-09-01 14:07:43 +0800

    test: add failing test for should_return_1_when_countOff_given_1()

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,17 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_1_when_countOff_given_1() {
+        String expected = "1";
+        FizzBuzz fizz = new FizzBuzz();
+
+        String actual = fizz.countOff(1);
+
+        assertEquals(expected, actual);
+    }
 }
